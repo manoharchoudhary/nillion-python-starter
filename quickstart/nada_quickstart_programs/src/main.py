9e90e1489934fbe10b14a11c76af4db984af9e6f
manoharchoudhary <mchoudhary2406@gmail.com>
2024-07-15 14:41:14 +0000

    my new nada program

--- quickstart/nada_quickstart_programs/src/main.py
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -2,32 +2,18 @@ from nada_dsl import *
 
 def nada_main():
     # Define parties
-    party1 = Party(name="Party1")
-    party2 = Party(name="Party2")
-    party3 = Party(name="Party3")
-    party4 = Party(name="Party4")
-    party5 = Party(name="Party5")
+    player = Party(name="Player")
+    game_master = Party(name="GameMaster")
+    result_party = Party(name="ResultParty")
 
-    # Define secret integers
-    a = SecretInteger(Input(name="A", party=party1))
-    b = SecretInteger(Input(name="B", party=party2))
-    c = SecretInteger(Input(name="C", party=party3))
-    d = SecretInteger(Input(name="D", party=party4))
+    # Game master provides the position of the ball (randomly decided outside of this code)
+    ball_position = SecretInteger(Input(name="BallPosition", party=game_master))  # 1, 2, or 3
+    guess = SecretInteger(Input(name="Guess", party=player))  # Player's guess (1, 2, or 3)
 
-    # Perform operations
-    sum_ab = a + b
-    diff_cd = c - d
-    prod_ac = a * c
-    div_bd = b / d
+    # Check if the guess is correct
+    is_correct = guess == ball_position
 
-    # Perform a combined operation
-    combined_result = (sum_ab * diff_cd) + prod_ac - div_bd
-
-    # Generate outputs
-    output1 = Output(sum_ab, "sum_ab_output", party5)
-    output2 = Output(diff_cd, "diff_cd_output", party5)
-    output3 = Output(prod_ac, "prod_ac_output", party5)
-    output4 = Output(div_bd, "div_bd_output", party5)
-    output5 = Output(combined_result, "combined_result_output", party5)
-
-    return [output1, output2, output3, output4, output5]
+    # Output the result
+    result = Output(is_correct, "GuessResult", result_party)
+    
+    return [result]
